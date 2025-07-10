6ce0fe23e1c5e7ec7c7f12daa28875dfeac542a5
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-10 12:14:22 -0400

    this is getting weird lol

--- __test__/ContactPage.test.tsx
+++ __test__/ContactPage.test.tsx
@@ -1,7 +1,7 @@
 // this will test the form and data and all tha
 import  ContactPage  from "../src/components/ContactPage"
-import { describe, it, expect,  } from "vitest"
-import { render, screen, fireEvent, waitFor } from "@testing-library/react"
+import { describe, it, expect } from "vitest"
+import { render, screen, fireEvent } from "@testing-library/react"
 
 
 describe('Contact Form',() => {
