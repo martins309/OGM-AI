3458c64d850ca6b23c880128554d05c997896eb0
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-10 10:29:43 -0400

    did the tests and now Im going to add the git hub actions and then we should be good to deploy

--- __test__/ContactPage.test.tsx
+++ __test__/ContactPage.test.tsx
@@ -1,7 +1,7 @@
 // this will test the form and data and all tha
 import  ContactPage  from "../src/components/ContactPage"
-import { describe, it, expect } from "vitest"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { describe, it, expect,  } from "vitest"
+import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 
 
 describe('Contact Form',() => {
@@ -10,11 +10,26 @@ describe('Contact Form',() => {
 
         expect(screen.getByLabelText(/Full Name */i)).toBeInTheDocument()
         expect(screen.getByLabelText(/Email Address /i)).toBeInTheDocument()
-        expect(screen.getByLabelText(/ Company /i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Company/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Service of Interest/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Message */i)).toBeInTheDocument()
+        expect(screen.getByRole('button', { name: /send message/i})).toBeInTheDocument()
 
 
     })
 
-     
+    it('displays error if the message is too short', async () => {
+        render(<ContactPage/>)
+        
+        fireEvent.change(screen.getByLabelText(/Full Name/i), {target: {value: "John Doe"}})
+        fireEvent.change(screen.getByLabelText(/Email Address/i), {target: {value: "john.doe@email.com"}})
+        fireEvent.change(screen.getByLabelText(/Message/i), {target: {value: "What is up"}})
+        fireEvent.change(screen.getByLabelText(/Security Question/i), {target: {value: "1000"}})
+
+        fireEvent.submit(screen.getByRole('button', { name: /send message/i}))
+
+       
+
+    })     
    
 })
