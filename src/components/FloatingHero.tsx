d4b62257df1f5299567284ceb78606c73b349260
Andrew Martins <andrew_omartins@yahoo.com>
2025-07-10 13:32:28 -0400

    please load my image

--- src/components/FloatingHero.tsx
+++ src/components/FloatingHero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import heroImage from '../assets/images/hero.jpg';
+import heroImage from '../assets/images/finalHeroAi.png';
 
 
 export default function FloatingHero() {
