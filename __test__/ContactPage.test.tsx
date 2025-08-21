fe990de97cf5598af935228101e31108bdcd367b
Andrew Martins <andrew_omartins@yahoo.com>
2025-08-21 13:20:03 -0400

    fixed the issue with the toBeInDocument linter

--- __test__/ContactPage.test.tsx
+++ __test__/ContactPage.test.tsx
@@ -2,7 +2,7 @@
 import  ContactPage  from "../src/components/ContactPage"
 import { describe, it, expect } from "vitest"
 import { render, screen, fireEvent } from "@testing-library/react"
-
+import '@testing-library/jest-dom'
 
 
 describe('Contact Form',() => {
